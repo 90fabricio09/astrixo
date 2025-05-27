49a1a4b91031a379071ce756ba42422557ff8c9f
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2025-05-27 11:15:17 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -236,9 +236,8 @@ export default function Home() {
         <div className="footer-bottom">
           <p>&copy; {new Date().getFullYear()} Astrixo - Agência de Desenvolvimento Web, Design Gráfico e Tráfego Orgânico. Todos os direitos reservados.</p>
           <div className="social-links">
-          <a href="https://wa.me/5511931501833" target="_blank" rel="noopener noreferrer"><i className="bi bi-whatsapp"></i></a>
+            <a href="https://wa.me/5511931501833" target="_blank" rel="noopener noreferrer"><i className="bi bi-whatsapp"></i></a>
             <a href="https://www.instagram.com/astrixo.web/" target="_blank" rel="noopener noreferrer"><i className="bi bi-instagram"></i></a>
-            <a href="https://discord.gg/8U3TBVfc6g" target="_blank" rel="noopener noreferrer"><i className="bi bi-discord"></i></a>
           </div>
         </div>
       </footer>
